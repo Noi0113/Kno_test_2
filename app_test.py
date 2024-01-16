25670b7529db2aaf5ccbfd52b76b067186ab4ad6
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-16 21:53:40 +0900

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -6,6 +6,12 @@ def main():
 #タイトル
 st.title('問題発見と解決テストサイト') 
 
+#CSVファイル読み込み
+import pandas as pd
+uploaded_file = st.file_uploader("CSVファイルを選択してください(まだファイルアップロードする場所を作っただけで、アップロードしても何も起こらないです)", type="csv")
+if uploaded_file is not None:
+    data = pd.read_csv(uploaded_file)
+
 #ボタン
 st.button('新規作成',use_container_width=True)
 st.button('対戦表の確認',use_container_width=True,help='ページ準備中')
