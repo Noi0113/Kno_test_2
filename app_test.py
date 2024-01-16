191c9d8e350ff64969a2594a97f165a020847fb0
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-16 18:19:55 +0900

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -9,7 +9,7 @@ st.title('問題発見と解決テストサイト')
 
 #CSVファイルをアップロード(とりあえず)
 import pandas as pd
-uploaded_file = st.file_uploader("CSVファイルを選択してください", type="csv")
+uploaded_file = st.file_uploader("CSVファイルを選択してください(まだファイルアップロードする場所を作っただけで、アップロードしても何も起こらないです)", type="csv")
 if uploaded_file is not None:
     data = pd.read_csv(uploaded_file)
 
