003fc0369d45e50fe61aea1c84e20adf79a86b1e
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-16 21:20:41 +0900

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -1,11 +1,10 @@
 import streamlit as st
+
+#######トップページ
 def main():
     status_area = st.empty()
- #ここから上は編集しない
-
 #タイトル
 st.title('問題発見と解決テストサイト')
-
 
 #CSVファイルをアップロード(とりあえず)
 import pandas as pd
@@ -14,12 +13,15 @@ if uploaded_file is not None:
     data = pd.read_csv(uploaded_file)
 
 #ボタン
-st.button('新規作成',use_container_width=True)
+st.sidebar.button('新規作成',use_container_width=True,on_click=new)
 st.button('対戦表の確認',use_container_width=True)
 
-
-
-
-#ここから下は編集しない
 if __name__ == '__main__':
     main()
+
+#######トップページ終わり
+
+#######新規作成クリック後のページ
+def new():
+    st.sidebar.title("ページが切り替わりました")
+    st.markdown("## 次のページです")
