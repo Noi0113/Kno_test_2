4fda2431b79d9ebb129a5c93674028cf9a7edb24
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-16 21:50:24 +0900

    Add files via upload

--- app_test.py
+++ app_test.py
@@ -1,20 +1,24 @@
+import time
+
 import streamlit as st
+
 
 def main():
     status_area = st.empty()
-#タイトル
-st.title('問題発見と解決テストサイト')
 
-#CSVファイルをアップロード(とりあえず)
-import pandas as pd
-uploaded_file = st.file_uploader("CSVファイルを選択してください//(まだファイルアップロードする場所を作っただけで、アップロードしても何も起こらないです。今後最適化を実験するときのために使えるかも)", type="csv")
-if uploaded_file is not None:
-    data = pd.read_csv(uploaded_file)
+    # カウントダウン
+    count_down_sec = 5
+    for i in range(count_down_sec):
+        # プレースホルダーに残り秒数を書き込む
+        status_area.write(f'{count_down_sec - i} sec left')
+        # スリープ処理を入れる
+        time.sleep(1)
 
-#ボタン
-st.button('新規作成',use_container_width=True)
-st.button('対戦表の確認',use_container_width=True,help='ページ準備中')
+    # 完了したときの表示
+    status_area.write('Done!')
+    # 風船飛ばす
+    st.balloons()
+
 
 if __name__ == '__main__':
     main()
-
