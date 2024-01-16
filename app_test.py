b89db94247496f670b335cc82eecffc5eed8dcbc
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-16 18:14:58 +0900

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -1,13 +1,19 @@
 import streamlit as st
 def main():
     status_area = st.empty()
-    
-st.title('問題発見と解決テストサイト')
-import pandas as pd
+ #ここから上は編集しない
 
-uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+#タイトル
+st.title('問題発見と解決テストサイト')
+
+
+#CSVファイルをアップロード(とりあえず)
+import pandas as pd
+uploaded_file = st.file_uploader("CSVファイルを選択してください", type="csv")
 if uploaded_file is not None:
     data = pd.read_csv(uploaded_file)
 
+
+#ここから下は編集しない
 if __name__ == '__main__':
     main()
