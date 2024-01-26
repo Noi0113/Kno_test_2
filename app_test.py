e46624a7f392f61e32ebff5f43bb148ca952bd76
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-26 15:07:01 +0900

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -17,11 +17,6 @@ st.link_button('新規作成',"https://monketsu-test-new-page.streamlit.app/",us
 st.button('対戦表の確認',use_container_width=True,help='ページ準備中')
 st.button('送信')
 
-##csvファイルをつくる
-with open('data25_2.csv', 'w', newline='')       
-##csvファイルの出力##
-st.write(data25_2)
-
 #######トップページ終わり
 
 #######新規作成クリック後のページ
