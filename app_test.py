267b4352beb31d5937a134da37d8d9dfa9af6137
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-30 06:16:05 +0900

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -13,7 +13,7 @@ st.title('問題発見と解決テストサイト')
 #    data = pd.read_csv(uploaded_file)
 
 #ボタン
-st.link_button('新規作成',"https://monketsu-test-new-page.streamlit.app/",use_container_width=True)
+st.link_button('新規作成',"https://monketsu-2ndpage.streamlit.app/",use_container_width=True)
 st.button('対戦表の確認',use_container_width=True,help='ページ準備中')
 st.button('送信')
 
