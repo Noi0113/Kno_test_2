603649053fe242220368dbbe8986b9f3cbf52245
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-30 15:27:40 +0900

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -62,52 +62,34 @@ st.button('送信')
 #機能の追加
 
 # ページの選択と表示
-menu1 = ["大会ログイン", "新規大会登録" ]
-choice1 = st.selectbox("選択してください",menu1)
-st.write("{}ページです".format(choice1))
+menu = ["大会ログイン", "新規大会登録" ]
+choice = st.selectbox("選択してください",menu)
+st.write("{}ページです".format(choice))
 
-if choice1 == "大会ログイン":
-	username1 = st.text_input("大会名を入力")
-	password1 = st.text_input("大会パスワードを入力してください",type='password')
+if choice == "大会ログイン":
+	username = st.text_input("大会名を入力")
+	password = st.text_input("大会パスワードを入力してください",type='password')
 	if st.button('ログイン'):
-		hashed_pswd1 = make_hashes(password1)
-		result1 = login_user(username1,check_hashes(password1,hashed_pswd1))
-		if result1:
-			st.success("大会名：{}でログインしました".format(username1))
-		else:
-			st.warning("大会名か大会パスワードが間違っています")
-
-#elif choice1 == "新規大会登録":
-	
-
-menu = ["ホーム", "大会ログイン", "新規大会登録"]
-choice = st.sidebar.selectbox("メニュー", menu)
-
-if choice == "ホーム":
-	st.subheader("ホーム画面です")
-elif choice == "大会ログイン":
-	st.subheader("大会ログイン画面です")
-
-	username = st.sidebar.text_input("大会名を入力してください")
-	password = st.sidebar.text_input("大会パスワードを入力してください", type='password')
-	if st.sidebar.checkbox("ログイン"):
 		hashed_pswd = make_hashes(password)
-		result = login_user(username, check_hashes(password, hashed_pswd))
+		result = login_user(username,check_hashes(password,hashed_pswd))
 		if result:
 			st.success("大会名：{}でログインしました".format(username))
 		else:
-	    		st.warning("大会名か大会パスワードが間違っています")
+			st.warning("大会名か大会パスワードが間違っています")
 
 elif choice == "新規大会登録":
-	st.subheader("新しい大会を作成します")
-	new_user = st.text_input("大会名を入力してください（被りがあると注意されます）")
-	new_password = st.text_input("大会パスワードを入力してください", type='password')
-
+	new_user = st.text_input("大会名を入力してください（被りがあると注意されて新規作成できない予定）")
+	new_password = st.text_input("大会パスワードを入力してください",type='password')
 	if st.button("登録"):
-    		create_user()
-    		add_user(new_user, make_hashes(new_password))
-    		st.success("新しい大会の作成に成功しました")
-    		st.info("大会ログイン画面からログインしてください")
+		past_user = add_user(new_user,make_hashes(new_password))
+		if past_user:
+
+		else:
+			create_user()
+			st.success("新しい大会の作成に成功しました")
+			st.info("大会ログイン画面からログインしてください")
+#---------------------------------------------------------------------------
+
 
 
 #######トップページ終わり
