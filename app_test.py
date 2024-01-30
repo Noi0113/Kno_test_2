fd9b4c27c401b7e23056b7860bc9f1f17173526d
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-30 14:28:50 +0900

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -6,6 +6,26 @@ import sqlite3
 import hashlib
 import pandas as pd
 
+#ここからログイン機能について
+def create_user():
+    c.execute('CREATE TABLE IF NOT EXISTS userstable (username TEXT PRIMARY KEY, password TEXT)')
+
+def add_user(username, password):
+    # ユーザーが既に存在するかを確認
+    c.execute('SELECT * FROM userstable WHERE username = ?', (username,))
+    existing_user = c.fetchone()
+    if existing_user:
+        st.warning("そのユーザー名は既に使用されています")
+    else:
+        c.execute('INSERT INTO userstable (username, password) VALUES (?, ?)', (username, password))
+        conn.commit()
+
+def login_user(username, password):
+    c.execute('SELECT * FROM userstable WHERE username = ? AND password = ?', (username, password))
+    data = c.fetchall()
+    return data
+#ここまでログイン機能について
+
 def main():
     status_area = st.empty()
 #タイトル
