ffbd2e685c529102bab81e4b551f2ba7ec205630
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-30 19:58:36 +0900

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -9,16 +9,12 @@ def main():
 #タイトル
 st.title('問題発見と解決テストサイト') 
 
-#CSVファイル読み込み
-#import pandas as pd
-#uploaded_file = st.file_uploader("CSVファイルを選択してください(まだファイルアップロードする場所を作っただけで、アップロードしても何も起こらないです)", type="csv")
-#if uploaded_file is not None:
-#    data = pd.read_csv(uploaded_file)
 
 #ボタン
 st.link_button('新規作成',"https://monketsu-2ndpage.streamlit.app/",use_container_width=True)
+st.button('アンケート回答',use_container_width=True,help='ページ準備中')
 st.button('対戦表の確認',use_container_width=True,help='ページ準備中')
-st.button('送信')
+
 
 ##ログインについて
 #st.link_button()を導入したい
