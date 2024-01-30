d911501eeeef37c8ab8e7b55bab22d8c86c7716d
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-30 20:54:46 +0900

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -12,7 +12,7 @@ st.title('問題発見と解決テストサイト')
 
 #ボタン
 st.link_button('新規作成',"https://monketsu-newcreate.streamlit.app/",use_container_width=True)
-st.link_button('アンケート回答',"https://monketsu-questionnaire.streamlit.app/,"use_container_width=True,help='ページ準備中')
+st.link_button('アンケート回答',"https://monketsu-questionnaire.streamlit.app/",use_container_width=True,help='ページ準備中')
 st.link_button('対戦表の確認',"https://monketsu-2ndpage.streamlit.app/",use_container_width=True)
 
 
