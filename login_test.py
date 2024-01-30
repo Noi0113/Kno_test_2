c6b8bc486ea19a0cca8f360e3dee9c5172ea208a
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-30 14:50:17 +0900

    Update login_test.py

--- login_test.py
+++ login_test.py
@@ -3,20 +3,31 @@ import hashlib
 import pandas as pd
 import sqlite3
 
-def create_user():
-    c.execute('CREATE TABLE IF NOT EXISTS userstable (username TEXT PRIMARY KEY, password TEXT)')
+menu = ["ホーム", "大会ログイン", "新規大会登録"]
+choice = st.sidebar.selectbox("メニュー", menu)
 
-def add_user(username, password):
-    # ユーザーが既に存在するかを確認
-    c.execute('SELECT * FROM userstable WHERE username = ?', (username,))
-    existing_user = c.fetchone()
-    if existing_user:
-        st.warning("そのユーザー名は既に使用されています")
-    else:
-        c.execute('INSERT INTO userstable (username, password) VALUES (?, ?)', (username, password))
-        conn.commit()
+if choice == "ホーム":
+	st.subheader("ホーム画面です")
+elif choice == "大会ログイン":
+	st.subheader("大会ログイン画面です")
 
-def login_user(username, password):
-    c.execute('SELECT * FROM userstable WHERE username = ? AND password = ?', (username, password))
-    data = c.fetchall()
-    return data
+	username = st.sidebar.text_input("大会名を入力してください")
+	password = st.sidebar.text_input("大会パスワードを入力してください", type='password')
+	if st.sidebar.checkbox("ログイン"):
+		hashed_pswd = make_hashes(password)
+		result = login_user(username, check_hashes(password, hashed_pswd))
+		if result:
+			st.success("大会名：{}でログインしました".format(username))
+		else:
+	    		st.warning("大会名か大会パスワードが間違っています")
+
+elif choice == "新規大会登録":
+	st.subheader("新しい大会を作成します")
+	new_user = st.text_input("大会名を入力してください（被りがあると注意されます）")
+	new_password = st.text_input("大会パスワードを入力してください", type='password')
+
+	if st.button("登録"):
+    		create_user()
+    		add_user(new_user, make_hashes(new_password))
+    		st.success("新しい大会の作成に成功しました")
+    		st.info("大会ログイン画面からログインしてください")
