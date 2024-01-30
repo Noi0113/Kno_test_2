c74532b3d3db1e90d284193399093411b606166f
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-01-30 15:16:55 +0900

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -70,7 +70,7 @@ if choice1 == "大会ログイン":
 	username1 = st.text_input("大会名を入力")
 	password1 = st.text_input("大会パスワードを入力してください",type='password')
 	if st.button('ログイン'):
-		hashed_pswd1 = mke_hashes(password1)
+		hashed_pswd1 = make_hashes(password1)
 		result1 = login_user(username,check_hashes(password1,hashed_pswd1))
 		if result1:
 			st.success("大会名：{}でログインしました".format(username1))
