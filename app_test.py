01b2c31e091cb06bd415b82501f6179e2c5be23d
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-02-02 04:57:35 +0900

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -9,18 +9,21 @@ def main():
 #タイトル
 st.title('競技かるた　対戦表作成サイト') 
 
+st.subheader('このサイトの使い方')
+st.markdown('1：新規作成')
+st.markdown('大会主催者は大会IDとパスワードを作成し、参加者に共有してください')
+st.markdown('2:アンケート回答')
+st.markdown('大会参加者は大会IDとパスワードでログインし、個人の情報を入力してください')
+st.markdown('3:対戦表の確認')
+st.markdown('個人アンケートの入力終了後、大会参加者は大会IDとパスワードを入力してください')
+st.markdown('対戦表が確認できます')
 
 #ボタン
 st.link_button('新規作成',"https://monketsu-newcreate.streamlit.app/",use_container_width=True)
 st.link_button('アンケート回答',"https://monketsu-questionnaire.streamlit.app/",use_container_width=True)
 st.link_button('対戦表の確認',"https://monketsu-2ndpage.streamlit.app/",use_container_width=True)
 
-st.subheader('このサイトの使い方')
-st.markdown('1：新規作成\n大会主催者は大会IDとパスワードを作成し、参加者に共有してください')
-st.markdown('2:アンケート回答')
-st.markdown('大会参加者は大会IDとパスワードでログインし、個人の情報を入力してください')
-st.markdown('3:対戦表の確認')
-st.markdown('個人アンケートの入力終了後、大会参加者は大会IDとパスワードを入力し、対戦表を作成してください')
+
 
 #######トップページ終わり
 
