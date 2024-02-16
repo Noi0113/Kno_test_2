c61bf923fb196c88bd1c806210469c3b4a5a5a3e
KNo0113 <156491810+Noi0113@users.noreply.github.com>
2024-02-16 19:58:25 +0900

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -12,19 +12,21 @@ st.markdown('') #空行の作成
 
 st.subheader('このサイトの使い方')
 st.markdown('①新規作成')
-st.markdown('大会主催者は大会IDとパスワードを作成し、参加者に共有してください')
+st.markdown('大会主催者が大会名とパスワード、その他必要情報を入力してください。')
+st.link_button('新規作成',"https://monketsu-newcreate.streamlit.app/",use_container_width=True)
+st.markdown('') #空行の作成
+
 st.markdown('②アンケート回答')
-st.markdown('大会参加者は大会IDとパスワードでログインし、個人の情報を入力してください')
+st.markdown('大会参加者は、大会主催者から共有された大会名とパスワードを用いて、自分の情報を入力してください')
+st.link_button('アンケート回答',"https://monketsu-questionnaire.streamlit.app/",use_container_width=True)
+st.markdown('') #空行の作成
+
 st.markdown('③対戦表の確認')
-st.markdown('個人アンケートの入力終了後、大会参加者は大会IDとパスワードを入力してください')
+st.markdown('大会主催者が、大会参加者がアンケートを入力した後に大会名とパスワードを入力することで')
 st.markdown('対戦表が確認できます')
+st.link_button('対戦表の確認',"https://monketsu-2ndpage.streamlit.app/",use_container_width=True)
 st.markdown('') #空行の作成
 #ボタン
-st.link_button('新規作成',"https://monketsu-newcreate.streamlit.app/",use_container_width=True)
-st.link_button('アンケート回答',"https://monketsu-questionnaire.streamlit.app/",use_container_width=True)
-st.link_button('対戦表の確認',"https://monketsu-2ndpage.streamlit.app/",use_container_width=True)
-
-
 
 #######トップページ終わり
 
